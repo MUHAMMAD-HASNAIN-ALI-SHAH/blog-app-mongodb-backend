6e73504f090f70a7e234bb82e624299ca2a44d2a
M-HASNAIN-ALI-SHAH <23-cs-51@students.uettaxila.edu.pk>
2025-06-05 18:03:59 +0500

    fixing the url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
 }));
 app.use(express.json());
